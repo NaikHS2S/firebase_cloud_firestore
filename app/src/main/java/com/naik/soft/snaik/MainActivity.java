85c0a1ed4ffd703071686c2013364bdd2a56f90e
hknaik988 <hknaik988@gmail.com>
2018-09-23 00:54:30 +0530

    first commit

--- app/src/main/java/com/naik/soft/snaik/MainActivity.java
+++ app/src/main/java/com/naik/soft/snaik/MainActivity.java
@@ -4,25 +4,34 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.naik.soft.snaik.model.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    public static final String DATA_BASE_NAME = "userdata";
-    private DatabaseReference mDatabase;
-    int userID = 0;
+    public static final String COLLECTION_NAME = "users";
+    private FirebaseFirestore db;
 
 
     @Override
@@ -32,14 +41,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        addDataChangeListener();
+        db = FirebaseFirestore.getInstance();
+        getUsersData();
+        dataChangeListener();
 
         findViewById(R.id.addUser).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                writeNewUser(String.valueOf(userID), ((EditText)findViewById(R.id.name)).getText().toString()
+                addOrUpdateContacts(((EditText)findViewById(R.id.name)).getText().toString()
                         , ((EditText)findViewById(R.id.email)).getText().toString()
                         , ((EditText)findViewById(R.id.phone)).getText().toString());
             }
@@ -51,86 +61,183 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                removeUser(String.valueOf(userID));
+                //removeUser();
+                deleteContact();
             }
         });
 
     }
 
-    private void removeUser(String userId) {
+    private void removeUsers() {
+        String documentPath = ((EditText)findViewById(R.id.documentPath)).getText().toString();
 
-        mDatabase.child(DATA_BASE_NAME).child(userId).setValue(null)
+        db.collection(COLLECTION_NAME).document(documentPath)
+                .delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        System.out.println("Data Remove Success");
-                        displayMessage("Data Remove Success");
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                        getUsersData();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        System.out.println("Data Remove Failure");
-                        displayMessage("Data Remove Failure");
+                        Log.w(TAG, "Error deleting document", e);
                     }
                 });
 
     }
 
-    private void addDataChangeListener() {
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference(DATA_BASE_NAME);
+    private void getUsersData() {
 
-        ref.addValueEventListener(new ValueEventListener() {
+        db.collection(COLLECTION_NAME)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            StringBuilder userData = new StringBuilder();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                userData.append(" id: ");
+                                userData.append(String.valueOf(document.getId()));
+                                userData.append(" data: ");
+                                userData.append(String.valueOf(document.getData()));
+                                userData.append("\n");
+                            }
+
+                            ((TextView)findViewById(R.id.textView)).setText(userData.toString());
+
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+    }
+
+    private void dataChangeListener() {
+
+        db.collection(COLLECTION_NAME)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot snapshots,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "listen:error", e);
+                            return;
+                        }
+
+                        for (DocumentChange dc : snapshots.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                Log.d(TAG, "New city: " + dc.getDocument().getData());
+                            }
+                        }
+
+                        getUsersData();
+
+                    }
+                });
+
+    }
+
+    private void contactsChangeListener() {
+        db.collection(COLLECTION_NAME).document("contacts").
+                addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+                                        @Nullable FirebaseFirestoreException e) {
+                        getUsersData();
+
+                    }
+                });
+
+    }
+
+
+
+    private void addNewDocument( final String name, String email, String phone) {
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", name);
+        user.put("email", email);
+        user.put("phone", phone);
+
+        // Add a new document with a generated ID
+        db.collection(COLLECTION_NAME)
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: "
+                                + documentReference.getId());
+                        displayMessage("DocumentSnapshot added with ID: "
+                                + documentReference.getId());
+
+                        getUsersData();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                        displayMessage("Error adding document");
+                    }
+                });
+
+    }
+
+    private void addOrUpdateContacts( final String name, String email, String phone) {
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", name);
+        user.put("email", email);
+        user.put("phone", phone);
+
+        // Add a new document with a generated ID
+        db.collection(COLLECTION_NAME)
+                .document("contacts").set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        displayMessage("success");
+                        getUsersData();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                System.out.println("onDataChange");
-                StringBuilder userData = new StringBuilder();
-                for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
-                    User user = childSnapshot.getValue(User.class);
-                    System.out.println("The read : "+ dataSnapshot.getKey()+":" + user.email +":id:"+user.id);
-                    userData.append("User Id: ");
-                    userData.append(String.valueOf(user.id));
-                    userData.append(" User name: ");
-                    userData.append(String.valueOf(user.username));
-                    userData.append(" User email: ");
-                    userData.append(String.valueOf(user.email));
-                    userData.append(" Phone: ");
-                    userData.append(String.valueOf(user.phone));
-                    userData.append("\n");
-                    userID = user.id + 1 ;
-                }
-                ((TextView)findViewById(R.id.textView)).setText(userData.toString());
-
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
+            public void onFailure(@NonNull Exception e) {
+                displayMessage("failure");
             }
         });
+
     }
 
-    private void writeNewUser(final String userId, final String name, String email, String phone) {
-        final User user = new User(name, email, phone, userID);
-        mDatabase.child(DATA_BASE_NAME).child(userId).setValue(user)
+    private void deleteContact() {
+
+        // Add a new document with a generated ID
+        db.collection(COLLECTION_NAME)
+                .document("contacts").delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        System.out.println("Write Success");
-                        displayMessage("Write Success");
+                        displayMessage("success");
+                        //getUsersData();
+
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        System.out.println("Write Failure");
-                        displayMessage("Write Failure");
-                    }
-                });
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                displayMessage("failure");
+            }
+        });
+
     }
+
 
 
     private void displayMessage(String message){
